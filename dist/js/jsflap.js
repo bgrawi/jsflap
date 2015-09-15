989e6441f78c5d490d7fafd909449e7e2f08ce13
unknown <onionhouseprod@gmail.com>
2015-09-15 14:18:00 -0400

    Added tracking code and fixed #7: png exporting too small on high dpi devices.

--- dist/js/jsflap.js
+++ dist/js/jsflap.js
@@ -222,6 +222,83 @@
                     element.style[property] = value;
                 }
             }
+            
+            /**
+             * Writes an image into a canvas taking into
+             * account the backing store pixel ratio and
+             * the device pixel ratio.
+             *
+             * @author Paul Lewis
+             * @param {Object} opts The params for drawing an image to the canvas
+             */
+            function drawImage(opts) {
+            
+                if(!opts.canvas) {
+                    throw("A canvas is required");
+                }
+                if(!opts.image) {
+                    throw("Image is required");
+                }
+            
+                // get the canvas and context
+                var canvas = opts.canvas,
+                    context = canvas.getContext('2d'),
+                    image = opts.image,
+            
+                // now default all the dimension info
+                    srcx = opts.srcx || 0,
+                    srcy = opts.srcy || 0,
+                    srcw = opts.srcw || image.naturalWidth,
+                    srch = opts.srch || image.naturalHeight,
+                    desx = opts.desx || srcx,
+                    desy = opts.desy || srcy,
+                    desw = opts.desw || srcw,
+                    desh = opts.desh || srch,
+                    auto = opts.auto,
+            
+                // finally query the various pixel ratios
+                    devicePixelRatio = window.devicePixelRatio || 1,
+                    backingStoreRatio = context.webkitBackingStorePixelRatio ||
+                                        context.mozBackingStorePixelRatio ||
+                                        context.msBackingStorePixelRatio ||
+                                        context.oBackingStorePixelRatio ||
+                                        context.backingStorePixelRatio || 1,
+            
+                    ratio = devicePixelRatio / backingStoreRatio;
+            
+                // ensure we have a value set for auto.
+                // If auto is set to false then we
+                // will simply not upscale the canvas
+                // and the default behaviour will be maintained
+                if (typeof auto === 'undefined') {
+                    auto = true;
+                }
+            
+                // upscale the canvas if the two ratios don't match
+                if (auto && devicePixelRatio !== backingStoreRatio) {
+            
+                    var oldWidth = canvas.width;
+                    var oldHeight = canvas.height;
+            
+                    canvas.width = oldWidth * ratio;
+                    canvas.height = oldHeight * ratio;
+            
+                    canvas.style.width = oldWidth + 'px';
+                    canvas.style.height = oldHeight + 'px';
+            
+                    // now scale the context to counter
+                    // the fact that we've manually scaled
+                    // our canvas element
+                    context.scale(ratio, ratio);
+            
+                }
+                
+                // Fill a white BG to fix transparency
+                context.fillStyle = "#FFFFFF";
+                context.fillRect(desx, desy, desw, desh);
+            
+                context.drawImage(image, srcx, srcy, srcw, srch, desx, desy, desw, desh);
+            }
 
             var svgClone;
 
@@ -257,10 +334,8 @@
                 image.src = imgsrc;
 
                 image.onload = function() {
-                    context.fillStyle = "#FFFFFF";
-                    context.fillRect(0, 0, width, height);
 
-                    context.drawImage(image, 0, 0);
+                    drawImage({image: image, canvas: canvas});
 
                     var canvasdata = canvas.toDataURL("image/png");
 
